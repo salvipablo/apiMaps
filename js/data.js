83cb02022d130a5012559d84591d9fe694216d8f
psalvi <salvipablo@gmail.com>
2022-07-14 14:46:33 -0300

    Customize bookmarks.
    Resolve: T#00007

---
+++ js/data.js
@@ -0,0 +1,58 @@
+const data = [
+  {
+    name: "Casa 1",
+    address: "Iriart 568",
+    realEstate: "Inmobiliaria 1",
+    phone: "3489 456789",
+    coordinates: { lat: -34.17332157599266, lng: -58.951970928130855 }
+  },
+  {
+    name: "Casa 2",
+    address: "Belgrano 568",
+    realEstate: "Inmobiliaria 1",
+    phone: "3489 456789",
+    coordinates: { lat: -34.183644979086196, lng: -58.94910336873899 }
+  },
+  {
+    name: "Casa 3",
+    address: "Castilla 568",
+    realEstate: "Inmobiliaria 1",
+    phone: "3489 456789",
+    coordinates: { lat: -34.162638569090376, lng: -58.97471621364243 }
+  },
+  {
+    name: "Casa 4",
+    address: "Peron 568",
+    realEstate: "Inmobiliaria 2",
+    phone: "3489 456789",
+    coordinates: { lat: -34.16657586792696, lng: -58.96815040203507 }
+  },
+  {
+    name: "Casa 5",
+    address: "Urquiza 568",
+    realEstate: "Inmobiliaria 2",
+    phone: "3489 456789",
+    coordinates: { lat: -34.16405208805553, lng: -58.96823887826371 }
+  },
+  {
+    name: "Casa 6",
+    address: "Castelli 568",
+    realEstate: "Inmobiliaria 2",
+    phone: "3489 456789",
+    coordinates: { lat: -34.164213887665575,lng: -58.96148675501967 }
+  },
+  {
+    name: "Casa 7",
+    address: "Chacabuco 568",
+    realEstate: "Inmobiliaria 3",
+    phone: "3489 451236",
+    coordinates: { lat: -34.16725647349148, lng: -58.957630863717256 }
+  },
+  {
+    name: "Casa 8",
+    address: "Ameghino 568",
+    realEstate: "Inmobiliaria 3",
+    phone: "3489 458782",
+    coordinates: { lat: -34.17008297134633, lng: -58.95803822258429 }
+  }
+];
